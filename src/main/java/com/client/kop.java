c80bd914d42fe582abb9aaf39ab276dce06fd669
John Doe <johh.doe@mail1.com>
2013-01-16 15:47:19 +0400

    added readme

--- src/main/java/com/client/kop.java
+++ src/main/java/com/client/kop.java
@@ -29,7 +29,7 @@ class ItemSupplyLocalDS extends DataSource {
 
     public ItemSupplyLocalDS(String id) {
 
-        //setID(id);pui!!!
+        //nvkjsfvewiruberwiob
         DataSourceIntegerField pkField = new DataSourceIntegerField("itemID");
         pkField.setHidden(true);
         pkField.setPrimaryKey(true);
